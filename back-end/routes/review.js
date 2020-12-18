d7e33ee826342f5efa897d4502c1bd379d454e78
Mariana Serban <marianaserban734@gmail.com>
2020-12-18 19:49:41 +0200

    fixed post+delete

--- back-end/routes/review.js
+++ back-end/routes/review.js
@@ -1,19 +1,27 @@
 const sequelize = require('../models/index.js')
 const express = require('express')
 const router = express.Router()
-
+const models=require('../models')
+const User=require('../models').User
 
 //adauga review
-router.post('/user/:pid/review', async (req, res) => {
+router.post('/user/:pid/review', async (req, res,next) => {
     try {
-        const review = await models.Review.create(req.body)
+        const person = await User.findByPk(req.params.pid)
+       if(person){
+
+        console.log(person.id)
+        const review = await models.Review.create({...req.body, userId: person.id})
+        
         if (review)
             res.status(201).json({ message: 'created' })
         else
             res.status(400).json({ message: 'failed' })
+       }
     } catch (error) {
         //Trebuie facut catch la erori la final in server.js (prin middleware ca la seminar)
-        next(error);
+        //next(error);
+        res.status(500).send({message:'crapa'})
     }
 })
 
@@ -33,14 +41,13 @@ router.put('/user/:pid/review/:rid', async (req, res) => {
         next(error);
     }
 
-
 })
 
 //stergere un anumit review
 router.delete('/user/:pid/review/:rid', async (req, res) => {
     try {
         const id = req.params.rid;
-        const review = await models.User.findByPk(id)
+        const review = await models.Review.findByPk(id)
         if (review) {
             await review.destroy();
             res.status(200).json({ message: "deleted" });
